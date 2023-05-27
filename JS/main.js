f242c66f7200ab5ac14d988a2af72822675df1d0
Danilo <rererr2008@gmail.com>
2023-05-27 16:15:12 +0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// function cl() {
+//     const conf = confirm()
+//     if (conf == true) {
+//         document.querySelector('.btn').classList.add('btn__active')
+//     }
+//     else{
+//         document.querySelector('.btn').classList.add('btn__active-2')
+//     }
+// }
+
+
+const anwers = []
+
+anwers[0] = prompt("Сколько вам лет" , "")
+anwers[1] = prompt("Как вас зовут" , "")
+anwers[2] = prompt("Какая ваша фамилия" , "")
+console.log("qwe");
+
+document.write(anwers)
